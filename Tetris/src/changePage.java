e41db49b0e940de5200500d55b9484b4620f5c1d
pinkbear505 <12110528@mail.sustech.edu.cn>
2022-05-08 17:54:08 +0800

    change

--- Tetris/src/changePage.java
+++ Tetris/src/changePage.java
@@ -6,6 +6,7 @@ public class changePage {
     public static void ChangePage(JButton button,JFrame nowPage,JFrame aimPage){
         button.addActionListener(new ActionListener() {
             @Override
+            //java
             public void actionPerformed(ActionEvent e) {
                 nowPage.dispose();
                 aimPage.setVisible(true);
